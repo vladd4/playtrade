0ec73c5412c673022bd9e6f6e9754808e553ce94
Vladyslav D <donetsvlad444@gmail.com>
2024-12-17 19:00:18 +0200

    change http to https url for bot menu

--- server/src/bot/service/menu.service.ts
+++ server/src/bot/service/menu.service.ts
@@ -5,29 +5,29 @@ import { Markup } from 'telegraf';
 export class MenuService {
   createMainMenu() {
     return Markup.inlineKeyboard([
-      [Markup.button.webApp('📦 Магазин', 'http://playtrade.pp.ua')],
+      [Markup.button.webApp('📦 Магазин', 'https://playtrade.pp.ua')],
       [
         Markup.button.webApp(
           '🔄 Продати ігровий товар',
-          'http://playtrade.pp.ua/profile/products/create-product',
+          'https://playtrade.pp.ua/profile/products/create-product',
         ),
       ],
       [
         Markup.button.webApp(
           '💬 Мої листування',
-          'http://playtrade.pp.ua/messages',
+          'https://playtrade.pp.ua/messages',
         ),
       ],
       [
         Markup.button.webApp(
           '👤 Мій аккаунт',
-          'http://playtrade.pp.ua/profile',
+          'https://playtrade.pp.ua/profile',
         ),
       ],
       [
         Markup.button.webApp(
           '📃 Мої товари',
-          'http://playtrade.pp.ua/profile/products',
+          'https://playtrade.pp.ua/profile/products',
         ),
       ],
     ]);
